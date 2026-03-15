c43bdcab7f00bd0b6994512c3938d3a98250e83e
nohimazin <139631966+nohimazin@users.noreply.github.com>
2026-03-15 09:20:53 +0900

    CompatShims.ktを追加し、データベース関連の型エイリアスを新しいパッケージ名に更新

---
+++ app/src/main/java/com/dd3boh/outertune/db/CompatShims.kt
@@ -0,0 +1,5 @@
+package com.dd3boh.outertune.db
+
+typealias InternalDatabase = com.nohimazin.monotune.db.InternalDatabase
+typealias MusicDatabase = com.nohimazin.monotune.db.MusicDatabase
+typealias DatabaseDao = com.nohimazin.monotune.db.DatabaseDao
