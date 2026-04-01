e9d4e06db137ca967d0a767a93ab715569bf91f7
nohimazin <139631966+nohimazin@users.noreply.github.com>
2026-04-01 18:53:34 +0900

    feat: initialize project dependencies with version catalog and add CI build workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,6 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
-}
 
 @file:Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
