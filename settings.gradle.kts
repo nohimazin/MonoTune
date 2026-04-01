8140557e75d4143a9e13ceed934b142cd37313d4
nohimazin <139631966+nohimazin@users.noreply.github.com>
2026-04-01 18:23:25 +0900

    feat: add GitHub Actions workflows for automated builds and enable mavenLocal in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
     repositories {
+        mavenLocal()
         google()
         mavenCentral()
         maven { setUrl("https://jitpack.io") }
